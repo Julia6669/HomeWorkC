4b80cee0f35ed9c5e4e9fce35ad4da5c1c3ef076
Julia <iulka.miavi@yandex.ru>
2023-03-14 17:48:11 +0300

    done task6

---
+++ Task6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0) 
+    Console.WriteLine("четное");
+ else 
+    Console.WriteLine("нечетное");
+Console.ReadLine();
+
