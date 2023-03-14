55bdf2293ed6a1df24ee5db5d8812922685469dc
Julia <iulka.miavi@yandex.ru>
2023-03-14 16:12:15 +0300

    done task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+System.Console.WriteLine("Большее число: " + a + ", меньшее число: " + b);
+else System.Console.WriteLine($"Большее число: {a}, меньшее число: {b}");
