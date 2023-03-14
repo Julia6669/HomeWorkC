67c9bf84be3d70fb0f4517277eb17b09dccdde6d
Julia <iulka.miavi@yandex.ru>
2023-03-14 22:11:24 +0300

    done task10

---
+++ Task10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+int n = new Random().Next(100, 999); //[100, 999]
+Console.WriteLine($"Случайно сгенирированное число {n}");
+int n1 = n / 100;
+int n3 = n % 10;
+Console.WriteLine(n1 * 10 + n3);
